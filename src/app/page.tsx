edc5b2826521be858b57240b4b13e13fd974eab4
Ahmed Karakaci <ahmed.karakaci@student.hogent.be>
2025-02-19 20:13:42 +0100

    added header and mini header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Button from "./components/button/Button";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      <h1> Ottieni il mio link di affiliazione Amazon</h1>
+      <h1 className={styles.header}> 
+        Ottieni il mio link di affiliazione Amazon
+      </h1>
+      <h2 className={styles.miniheader}> Prima che Mariano distrugga il bottone...</h2>
       <Button />
     </main>
   );
