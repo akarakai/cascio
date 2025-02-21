14e1525bc53f6d8cb56b107aedc563a280f7459e
Ahmed Karakaci <ahmed.karakaci@student.hogent.be>
2025-02-21 10:26:33 +0100

    volta buona con head, modifica metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Head from "next/head";
 import Minigame from "./components/minigame/Minigame";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
+      <Head>
+          <title>Link Affiliazione Amazon </title>
+          <meta name="description" content="Ottieni il mio link di affiliazione Amazon prima che Mariano distrugga il bottone!" />
+          <meta name="keywords" content="Amazon affiliazione, guadagna con Amazon, link di affiliazione, offerte Amazon, minigioco Amazon, Mariano bottone, gioco divertente, Boris" />
+          <meta name="author" content="akarakai" />
+          <meta property="og:title" content="Link Affiliazione Amazon"/>
+          <meta property="og:description" content="Riuscirai a prendere il link di affiliazione Amazon prima che Mariano distrugga il bottone? Gioca ora!" />
+          <meta property="og:type" content="website" />
+          <meta name="robots" content="index, follow" />
+      </Head>
       <main className={styles.main}>
         <section>
           <h1 className={styles.header}>Ottieni il mio Link di Affiliazione Amazon</h1>
