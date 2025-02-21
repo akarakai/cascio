a319e39371e27c7ff3d4ddbcf12fa05d38d1da53
Ahmed Karakaci <ahmed.karakaci@student.hogent.be>
2025-02-21 10:01:49 +0100

    change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Link Affiliazione Amazon - Vinci Prima che Mariano Distrugga il Bottone!</title>
+        <title>Link Affiliazione Amazon </title>
         <meta name="description" content="Ottieni il mio link di affiliazione Amazon prima che Mariano distrugga il bottone!" />
         <meta name="keywords" content="Amazon affiliazione, guadagna con Amazon, link di affiliazione, offerte Amazon, minigioco Amazon, Mariano bottone, gioco divertente, Boris" />
         <meta name="author" content="akarakai" />
-        <meta property="og:title" content="Link Affiliazione Amazon - Vinci Prima che Mariano Distrugga il Bottone!" />
+        <meta property="og:title" content="Link Affiliazione Amazon"/>
         <meta property="og:description" content="Riuscirai a prendere il link di affiliazione Amazon prima che Mariano distrugga il bottone? Gioca ora!" />
         <meta property="og:type" content="website" />
         <meta name="robots" content="index, follow" />
